25a707474b93626b18aea47ade8749aaf95f9bd9
Yaroslav82 <yargay230607@gmail.com>
2023-09-20 13:27:35 +0300

    Add delete msg and no id msg

--- src/main/java/org/example/app/utils/Constants.java
+++ src/main/java/org/example/app/utils/Constants.java
@@ -7,4 +7,7 @@ public final class Constants {
     public final static String DB_USER = "dev";
 
     public final static String DATA_INSERT_MSG = "\n>> Created.";
+    public final static String DATA_DELETE_MSG = "\n>> Deleted.";
+
+    public final static String ID_NO_EXISTS_MSG = "\n>> There is no such ID.";
 }
