808c6108e0acd1eaa9695e9258dce528d4330dfd
Yaroslav82 <yargay230607@gmail.com>
2023-09-20 14:26:15 +0300

    Create UserUpdateController

---
+++ src/main/java/org/example/app/controllers/UserUpdateController.java
@@ -0,0 +1,16 @@
+package org.example.app.controllers;
+
+import lombok.AllArgsConstructor;
+import org.example.app.services.UserUpdateService;
+import org.example.app.views.UserUpdateView;
+
+@AllArgsConstructor
+public class UserUpdateController {
+
+    private final UserUpdateService service;
+    private final UserUpdateView view;
+
+    public void updateUser() {
+        view.getOutput(service.updateUser(view.getData()));
+    }
+}
