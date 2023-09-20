fe335895bfe913f0628588a1efec1fc31b0dcb2e
Yaroslav82 <yargay230607@gmail.com>
2023-09-20 13:22:14 +0300

    Change snake case to camel case in HQL insert

--- INFO/SQLs.sql
+++ INFO/SQLs.sql
@@ -25,9 +25,9 @@ CREATE TABLE IF NOT EXISTS users
 );
 
 -- HQL
-INSERT INTO User (userName, firstName, lastName, email) VALUES (:user_name, :firstName, :lastName, :email)
+INSERT INTO User (userName, firstName, lastName, email) VALUES (:userName, :firstName, :lastName, :email)
 -- SQL
-INSERT INTO users (userName, firstName, lastName, email) VALUES (:user_name, :firstName, :lastName, :email))
+INSERT INTO users (user_name, first_name, last_name, email) VALUES (:user_name, :first_name, :last_name, :email))
 
 
 -- HQL
