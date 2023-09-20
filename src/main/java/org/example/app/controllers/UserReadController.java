ae620bc3cd20f07796eb03e6c33774c276b114b7
Yaroslav82 <yargay230607@gmail.com>
2023-09-20 14:24:20 +0300

    Create UserReadController

---
+++ src/main/java/org/example/app/controllers/UserReadController.java
@@ -0,0 +1,16 @@
+package org.example.app.controllers;
+
+import lombok.AllArgsConstructor;
+import org.example.app.services.UserReadService;
+import org.example.app.views.UserReadView;
+
+@AllArgsConstructor
+public class UserReadController {
+
+    private final UserReadService service;
+    private final UserReadView view;
+
+    public void readUsers() {
+        view.getOutput(service.readUsers());
+    }
+}
