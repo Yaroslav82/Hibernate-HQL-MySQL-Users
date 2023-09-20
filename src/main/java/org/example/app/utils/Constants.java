6adb7449cc14b7266b06c522d8f83ebc43be1a94
Yaroslav82 <yargay230607@gmail.com>
2023-09-20 13:51:33 +0300

    Add email constants

--- src/main/java/org/example/app/utils/Constants.java
+++ src/main/java/org/example/app/utils/Constants.java
@@ -6,9 +6,13 @@ public final class Constants {
     public final static String DB_URL = "jdbc:mysql://localhost:3306/demo_db?";
     public final static String DB_USER = "dev";
 
+    public final static String EMAIL_RGX = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+    public final static String WRONG_EMAIL_MSG = "Wrong email input.";
+
     public final static String DATA_INSERT_MSG = "\n>> Created.";
     public final static String DATA_UPDATE_MSG = "\n>> Updated.";
     public final static String DATA_DELETE_MSG = "\n>> Deleted.";
 
+    public final static String INPUT_REQ_MSG = "Input required.";
     public final static String ID_NO_EXISTS_MSG = "\n>> There is no such ID.";
 }
