cd508565e8b0f701ebde35836a37c9ebb189cc7e
Yaroslav82 <yargay230607@gmail.com>
2023-09-20 13:14:50 +0300

    Create Constants class

---
+++ src/main/java/org/example/app/utils/Constants.java
@@ -0,0 +1,8 @@
+package org.example.app.utils;
+
+public final class Constants {
+
+    public final static String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public final static String DB_URL = "jdbc:mysql://localhost:3306/demo_db?";
+    public final static String DB_USER = "dev";
+}
