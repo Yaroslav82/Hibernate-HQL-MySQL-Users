cc8a421e996b85b1f0d94e5a226e7b7ed6c23b8a
Yaroslav82 <yargay230607@gmail.com>
2023-09-20 13:30:00 +0300

    Create UserReadRepository

---
+++ src/main/java/org/example/app/repositories/UserReadRepository.java
@@ -0,0 +1,26 @@
+package org.example.app.repositories;
+
+import org.example.app.entities.User;
+import org.example.app.utils.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.Collections;
+import java.util.List;
+
+public class UserReadRepository {
+
+    public List<User> readUsers() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction transaction;
+
+            transaction = session.beginTransaction();
+            List<User> contacts = session.createQuery("FROM Contact", User.class).list();
+
+            transaction.commit();
+            return contacts;
+        } catch (Exception e) {
+            return Collections.emptyList();
+        }
+    }
+}
