c3b184b4d733671c94f6108f780c007bfc8ee945
Yaroslav82 <yargay230607@gmail.com>
2023-09-20 13:19:18 +0300

    Add insert message constant

--- src/main/java/org/example/app/utils/Constants.java
+++ src/main/java/org/example/app/utils/Constants.java
@@ -5,4 +5,6 @@ public final class Constants {
     public final static String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     public final static String DB_URL = "jdbc:mysql://localhost:3306/demo_db?";
     public final static String DB_USER = "dev";
+
+    public final static String DATA_INSERT_MSG = "\n>> Created.";
 }
