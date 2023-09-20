19a7f0df1fe779ab7ea135c3381751666869c9ed
Yaroslav82 <yargay230607@gmail.com>
2023-09-20 14:22:14 +0300

    Create UserDeleteController

---
+++ src/main/java/org/example/app/controllers/UserDeleteController.java
@@ -0,0 +1,16 @@
+package org.example.app.controllers;
+
+import lombok.AllArgsConstructor;
+import org.example.app.services.UserDeleteService;
+import org.example.app.views.UserDeleteView;
+
+@AllArgsConstructor
+public class UserDeleteController {
+
+    private final UserDeleteService service;
+    private final UserDeleteView view;
+
+    public void deleteUser() {
+        view.getOutput(service.deleteUser(view.getData()));
+    }
+}
