4007f2e41ab295939c795b8a7f7d0901323eb562
Yaroslav82 <yargay230607@gmail.com>
2023-09-20 14:42:48 +0300

    Add close message

--- src/main/java/org/example/app/utils/Constants.java
+++ src/main/java/org/example/app/utils/Constants.java
@@ -17,6 +17,7 @@ public final class Constants {
     public final static String DATA_UPDATE_MSG = "\n>> Updated.";
     public final static String DATA_DELETE_MSG = "\n>> Deleted.";
 
+    public final static String APP_CLOSE_MSG = "\n>> App closed.";
     public final static String INPUT_REQ_MSG = "Input required.";
     public final static String INCORRECT_VALUE_MSG = ">> Incorrect value! Try again.";
     public final static String ID_NO_EXISTS_MSG = "\n>> There is no such ID.";
