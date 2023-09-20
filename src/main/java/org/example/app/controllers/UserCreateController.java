bb181b94e580df67ee64a83618263ac2242a4ad1
Yaroslav82 <yargay230607@gmail.com>
2023-09-20 14:20:49 +0300

    Create UserCreateController

---
+++ src/main/java/org/example/app/controllers/UserCreateController.java
@@ -0,0 +1,16 @@
+package org.example.app.controllers;
+
+import lombok.AllArgsConstructor;
+import org.example.app.services.UserCreateService;
+import org.example.app.views.UserCreateView;
+
+@AllArgsConstructor
+public class UserCreateController {
+
+    private final UserCreateService service;
+    private final UserCreateView view;
+
+    public void createUser() {
+        view.getOutput(service.createUser(view.getData()));
+    }
+}
